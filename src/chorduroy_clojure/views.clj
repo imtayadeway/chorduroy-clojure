bee005b6dc344f6f3cb9c05690acb9bf09391e7e
Tim Wade <hello@timjwade.com>
2017-09-23 13:29:18 -0700

    Leverage the form API a bit more

--- src/chorduroy_clojure/views.clj
+++ src/chorduroy_clojure/views.clj
@@ -1,5 +1,5 @@
 (ns chorduroy-clojure.views
-  (:use [hiccup core page]))
+  (:use [hiccup core page form]))
 
 (def strings ["6th" "5th" "4th" "3rd" "2nd" "1st"])
 (def pitches ["A" "A#/Bb" "B" "C" "C#/Db" "D" "D#/Eb" "E" "F" "F#/Gb" "G" "G#/Ab"])
@@ -12,8 +12,6 @@
    [:body
     (form-to [:get "/results"]
              (for [string strings]
-               (list [:label {:for (str string "_String")} (str string " String")]
-                     [:select {:id string :name string}
-                      (for [pitch pitches]
-                        [:option {:value pitch} pitch])])))
-    [:input {:name "commit" :value "Submit" :data-disable-with "Submit" :type "Submit"}]]])
+               (list (label {:for (str string "_String")} (str string "_String") (str string " String"))
+                     (drop-down {:id string :name string} string pitches)))
+             (submit-button {:name "commit" :value "Submit" :data-disable-with "Submit" :type "Submit"} "Submit"))]))
