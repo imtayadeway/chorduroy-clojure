9bd84b95f97eac30fc0fde1f4f044e848a823646
Tim Wade <hello@timjwade.com>
2017-09-23 15:53:09 -0700

    Generate all major/minor chords

--- src/chorduroy_clojure/generator.clj
+++ src/chorduroy_clojure/generator.clj
@@ -1,5 +1,12 @@
 (ns chorduroy-clojure.generator)
 
+(def standard-tuning ["E" "A" "D" "G" "B" "E"])
+(def the-major-and-minor-chords
+  (set (for [pitch ["A" "A#/Bb" "B" "C" "C#/Db" "D" "D#/Eb" "E" "F" "F#/Gb" "G" "G#/Ab"]
+             harmony ["major" "minor"]]
+         (str pitch " " harmony))))
+
 (defn generate
   [strings]
-  [{:name "A major" :charts ["--0--\n--0--\n--0--\n--0--\n--0--\n--0--"]}])
+  (map (fn [chord] {:name chord :charts ["--0--\n--0--\n--0--\n--0--\n--0--\n--0--"]})
+       the-major-and-minor-chords))
--- test/chorduroy_clojure/generator_test.clj
+++ test/chorduroy_clojure/generator_test.clj
@@ -2,9 +2,7 @@
   (:require [clojure.test :refer :all]
             [chorduroy-clojure.generator :refer :all]))
 
-(def standard-tuning ["E" "A" "D" "G" "B" "E"])
-
 (deftest test-chord-entries
   (let [results (generate standard-tuning)
         chords (map :name results)]
-    (is (= ["A major"] chords))))
+    (is (= the-major-and-minor-chords (set chords)))))
