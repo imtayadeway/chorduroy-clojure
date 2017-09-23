b8464cb66843bd027c89a20b9008e64efbbb0c1b
Tim Wade <hello@timjwade.com>
2017-09-23 14:57:03 -0700

    Add a basic results page

--- src/chorduroy_clojure/views.clj
+++ src/chorduroy_clojure/views.clj
@@ -15,3 +15,12 @@
                (list (label {:for (str string "_String")} (str string "_String") (str string " String"))
                      (drop-down {:id string :name (clojure.string/lower-case string)} string pitches)))
              (submit-button {:name "commit" :value "Submit" :data-disable-with "Submit" :type "Submit"} "Submit"))]))
+
+(defn results-page
+  []
+  (html5
+   [:head
+    [:title "Chorduroy"]]
+   [:body
+    [:h2 "A major"]
+    [:pre "--0--\n--0--\n--0--\n--0--\n--0--\n--0--"]]))
