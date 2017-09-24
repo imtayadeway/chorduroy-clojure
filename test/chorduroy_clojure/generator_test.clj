b7bf023127269cd69732a56900302f7bc9752ee1
Tim Wade <hello@timjwade.com>
2017-09-24 11:42:59 -0700

    Move generate-test

--- test/chorduroy_clojure/generator_test.clj
+++ test/chorduroy_clojure/generator_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [chorduroy-clojure.generator :refer :all]))
 
-(deftest generate-test
-  (let [results (generate standard-tuning)
-        chords (map :name results)]
-    (is (= the-major-and-minor-chords (set chords)))))
 (deftest name-for-chord-test
   (is (= "A Major" (name-for-chord {:pitch "A" :tonality "Major"})))
   (is (= "A Minor" (name-for-chord {:pitch "A" :tonality "Minor"}))))
@@ -15,3 +11,8 @@
         g-chords (positions-for-chord {:pitch "G" :tonality "Minor"} standard-tuning)]
     (is (some #{[0 2 2 1 0 0]} e-chords))
     (is (some #{[3 2 0 0 0 3]} g-chords))))
+
+(deftest generate-test
+  (let [results (generate standard-tuning)
+        chords (map name-for-chord results)]
+    (is (= the-major-and-minor-chords (set chords)))))
