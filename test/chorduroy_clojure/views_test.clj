fbe4f85e1462ba3e7d5ff64b3a127feaba7c8a08
Tim Wade <hello@timjwade.com>
2017-09-30 13:19:54 -0700

    Render nils as xs

--- test/chorduroy_clojure/views_test.clj
+++ test/chorduroy_clojure/views_test.clj
@@ -4,4 +4,5 @@
 
 (deftest position-to-chart-test
   (is (= "--0--\n--0--\n--1--\n--2--\n--2--\n--0--" (position-to-chart [0 2 2 1 0 0])))
-  (is (= "--3--\n--0--\n--0--\n--0--\n--2--\n--3--" (position-to-chart [3 2 0 0 0 3]))))
+  (is (= "--3--\n--0--\n--0--\n--0--\n--2--\n--3--" (position-to-chart [3 2 0 0 0 3])))
+  (is (= "--2--\n--3--\n--2--\n--0--\n--x--\n--x--" (position-to-chart [nil nil 0 2 3 2]))))
