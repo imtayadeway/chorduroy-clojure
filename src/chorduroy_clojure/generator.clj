3c2aebf5efc696325048df2a7bc55dde8473f922
Tim Wade <hello@timjwade.com>
2018-08-24 15:52:03 -0700

    Filter the results a bit

--- src/chorduroy_clojure/generator.clj
+++ src/chorduroy_clojure/generator.clj
@@ -1,13 +1,15 @@
 (ns chorduroy-clojure.generator)
 (require '[chorduroy-clojure.core :refer :all])
+(require '[chorduroy-clojure.filterer :as filterer])
 
 (defn generate
   [tuning]
   (reduce (fn [result position]
             (if-let [chord (identify position tuning)]
-              (let [name (name-for-chord chord)]
-                (assoc result name (conj (get result name []) position)))
-
+              (if (filterer/eligible? chord (map #(assoc {} :open %1 :fret %2) tuning position))
+                (let [name (name-for-chord chord)]
+                  (assoc result name (conj (get result name []) position)))
+                result)
               result))
           {}
           all-playable-positions))
