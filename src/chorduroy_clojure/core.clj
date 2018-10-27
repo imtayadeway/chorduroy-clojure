6bf34cb2b3cadfd2752b457e94f79707cbc10dcd
Tim Wade <hello@timjwade.com>
2018-10-27 10:36:46 -0700

    Introduce the idea of optional notes

--- src/chorduroy_clojure/core.clj
+++ src/chorduroy_clojure/core.clj
@@ -5,18 +5,18 @@
   ["A" "A#/Bb" "B" "C" "C#/Db" "D" "D#/Eb" "E" "F" "F#/Gb" "G" "G#/Ab"])
 
 (def intervals
-  {"Major" [0 4 7]
-   "Minor" [0 3 7]
-   "Minor 7th" [0 3 7 10]
-   "Minor 9th" [0 3 7 14]
-   "Minor 11th" [0 3 7 17]
-   "Minor 13th" [0 3 7 20]
-   "Dominant 7th" [0 4 7 10]
-   "Major 7th" [0 4 7 11]
-   "Major 9th" [0 4 7 14] ;; need to start representing "optional" intervals
-   "Major 11th" [0 4 7 17]
-   "Major 13th" [0 4 7 21]
-   "Mystic Chord" [0 6 10 16 21 26]})
+  {"Major" {:required [0 4 7]}
+   "Minor" {:required [0 3 7]}
+   "Minor 7th" {:required [0 3 7 10]}
+   "Minor 9th" {:required [0 3 7 14] :optional [10]}
+   "Minor 11th" {:required [0 3 7 17] :optional [10 14]}
+   "Minor 13th" {:required [0 3 7 20] :optional [10 14 17]}
+   "Dominant 7th" {:required [0 4 7 10]}
+   "Major 7th" {:required [0 4 7 11]}
+   "Major 9th" {:required [0 4 7 14] :optional [11]}
+   "Major 11th" {:required [0 4 7 17] :optional [11 14]}
+   "Major 13th" {:required [0 4 7 21] :optional [11 14 17]}
+   "Mystic Chord" {:required [0 6 10 16 21 26]}})
 
 (defn name-for-chord
   [chord]
@@ -34,15 +34,19 @@
 (def the-diatonic-chords
   (vec (for [root the-chromatic-scale
              tonality (keys intervals)]
-         {:root root
-          :tonality tonality
-          :notes (map (partial walk-scale root) (get intervals tonality))} )))
+         (let [map-fn (partial walk-scale root)
+               required (map map-fn (get-in intervals [tonality :required]))
+               optional (map map-fn (get-in intervals [tonality :optional] []))]
+           {:root root
+            :tonality tonality
+            :required required
+            :optional optional}))))
 
 (defn identify
   [position tuning]
   (let [notes (remove nil? (map walk-scale tuning position))
         root (first notes)
-        candidates (filter #(and (= (set notes) (set (:notes %)))
+        candidates (filter #(and (= (set notes) (set (:required %)))
                                  (= root (:root %)))
                            the-diatonic-chords)]
     (first candidates)))
