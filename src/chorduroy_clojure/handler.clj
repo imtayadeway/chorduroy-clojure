6293ae5454db78ccfc245d884b530b081379a9b1
Tim Wade <hello@timjwade.com>
2018-11-24 11:31:13 -0800

    HACK: stream the results

--- src/chorduroy_clojure/handler.clj
+++ src/chorduroy_clojure/handler.clj
@@ -1,17 +1,32 @@
 (ns chorduroy-clojure.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.adapter.jetty :refer [run-jetty]]
+            [org.httpkit.server :refer :all]
+            [org.httpkit.timer :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [chorduroy-clojure.views :as views]
             [chorduroy-clojure.generator :as generator])
   (:gen-class))
 
+(defn results-handler
+  [request]
+  (let [{{:strs [sixth fifth fourth third second first]} :form-params} request]
+    (with-channel request channel
+      (loop [id 0]
+        (when (< id 10)
+          (schedule-task (* id 5000)
+                         (send! channel "<p><em>generating....</em></p>" false))
+          (recur (inc id))))
+      (schedule-task 60000 (close channel))
+      (send! channel
+             (->> [sixth fifth fourth third second first]
+                  generator/generate
+                  views/results-page)
+             true))))
+
 (defroutes app-routes
   (GET "/" [] (views/index-page))
-  (POST "/results"
-        {{:strs [sixth fifth fourth third second first]} :form-params}
-        (views/results-page (generator/generate [sixth fifth fourth third second first])))
+  (POST "/results" [] results-handler)
   (route/not-found "Not Found"))
 
 (def app
@@ -21,4 +36,4 @@
 (defn -main
   []
   (let [port (Integer. (or (System/getenv "PORT") 8080))]
-    (run-jetty app {:port port})))
+    (run-server app {:port port})))
